f0130f8cf9d9b774dda4147bf2dcc0dcf3e9fb10
john <johnsantosh2@gmail.com>
2023-03-13 20:24:50 +0530

    basic structure created

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-// Accessing elements 
+// Accessing elements
 
 let addBtn = document.querySelector("#task-add");
 let removeText = document.querySelector("#remove-taskText");
@@ -7,77 +7,89 @@ let task = document.querySelector("#parent-input");
 
 let deleteIcon = document.querySelector("#deleteTask");
 
-// adding a task 
-addBtn.addEventListener("click",preChecking);
+// adding a task
+addBtn.addEventListener("click", preChecking);
 
-// removing text from the input field 
+// removing text from the input field
 removeText.addEventListener("click", clearParentText);
 function clearParentText() {
-    task.value = "";
+  task.value = "";
 }
 
-// checking input field for empty input box before processing 
-function preChecking(){
-    return task.value ? createTask() : alert("No Task added")
+// checking input field for empty input box before processing
+function preChecking() {
+  return task.value ? createTask() : alert("No Task added");
 }
 
-// creating the task 
-function createTask(){
-    // creating elements 
-    let div = document.createElement('div')
-    let input = document.createElement('input')
-    let editButton = document.createElement("button");
-    let deleteTaskButton = document.createElement('button')
-    let saveEditButton = document.createElement('button')
+// creating the task
+function createTask() {
+  // creating elements
+  let div = document.createElement("div");
+  let input = document.createElement("input");
+  let editButton = document.createElement("button");
+  let deleteTaskButton = document.createElement("button");
+  let saveEditButton = document.createElement("button");
 
-    // adding attributes and textContent to the newly created elements 
-    div.classList.add("child")
+  // adding attributes and textContent to the newly created elements
+  div.classList.add("child");
 
-    input.classList.add("col-10")
-    input.value=task.value
-    input.disabled=true
-    
-    editButton.classList.add("col-1");
-    editButton.setAttribute('id', "editTask");
-    editButton.textContent = "✏️";
-    
-    deleteTaskButton.classList.add("col-1")
-    deleteTaskButton.id = "deleteTask";
-    deleteTaskButton.textContent="🗑️"
-    deleteTaskButton.addEventListener("click", ()=>div.remove());
+  input.classList.add("col-10");
+  input.value = task.value;
+  input.disabled = true;
 
-    saveEditButton.classList.add("col-1")
-    saveEditButton.textContent="✔️"
-    
-    editButton.addEventListener("click",()=>{
-        input.disabled=false;
-        input.classList.remove("col-10");
-        input.classList.add("col-9");
-        // making the done button (✔️) visible once the user click edit button(✏️)
-        saveEditButton.classList.remove('d-none')
-        // creating an done button (✔️) once the user click the edit icon (✏️)
-        div.firstElementChild.after(saveEditButton);
+  editButton.classList.add("col-1");
+  editButton.setAttribute("id", "editTask");
+  editButton.textContent = "✏️";
 
-        // done button (✔️) actions 
-        saveEditButton.addEventListener("click", function () {
-            input.disabled=true;
-            // hiding the edit button once the user clicked (✔️) icon 
-            saveEditButton.classList.add('d-none')
-            input.classList.remove("col-9");
-            input.classList.add("col-10");
-        });
-    })
+  deleteTaskButton.classList.add("col-1");
+  deleteTaskButton.id = "deleteTask";
+  deleteTaskButton.textContent = "🗑️";
+  deleteTaskButton.addEventListener("click", () => div.remove());
 
-    div.appendChild(input)
-    div.appendChild(editButton);
-    div.appendChild(deleteTaskButton)
-    parent.appendChild(div)
+  saveEditButton.classList.add("col-1");
+  saveEditButton.textContent = "✔️";
 
-    // clearing the text inside input field when task is added 
-    clearParentText();
+  editButton.addEventListener("click", () => {
+    input.disabled = false;
+    input.classList.remove("col-10");
+    input.classList.add("col-9");
+    // making the done button (✔️) visible once the user click edit button(✏️)
+    saveEditButton.classList.remove("d-none");
+    // creating an done button (✔️) once the user click the edit icon (✏️)
+    div.firstElementChild.after(saveEditButton);
+
+    // done button (✔️) actions
+    saveEditButton.addEventListener("click", function () {
+      input.disabled = true;
+      // hiding the edit button once the user clicked (✔️) icon
+      saveEditButton.classList.add("d-none");
+      input.classList.remove("col-9");
+      input.classList.add("col-10");
+    });
+  });
+
+  div.appendChild(input);
+  div.appendChild(editButton);
+  div.appendChild(deleteTaskButton);
+  parent.appendChild(div);
+
+  // clearing the text inside input field when task is added
+  clearParentText();
 }
-
 
 // let currentYear = ;
-let footer = document.querySelector('footer p')
+let footer = document.querySelector("footer p");
 footer.innerHTML = `Copyright &copy; ${new Date().getFullYear()} John santhosh`;
+
+let newBtn = document.querySelector(".newNote");
+newBtn.addEventListener("click", () => {
+  let newList = new DocumentFragment();
+});
+
+function getdate() {
+  let date = new Date();
+  console.log(date);
+  console.log(date.getDate(), date.getMonth(), date.toLocaleTimeString());
+}
+
+getdate();
